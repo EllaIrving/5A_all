e7541b392bc040f9f7e43c3644547dc0ccca7ee6
Ella Irving <eirving@meca.edu>
2024-12-03 18:00:53 -0500

    hello

---
+++ script.js
@@ -0,0 +1,19 @@
+const progressTag = document.querySelector(".progress_bar");
+const bodyTag = document.querySelector("body");
+
+// Update Progress Bar
+document.addEventListener("scroll", function () {
+
+    // get distance scrolled and turn it into an integer
+    const pixelsScrolled = window.scrollY;
+
+    // retrieve page height
+    const pageHeight = bodyTag.getBoundingClientRect().height;
+
+    // subtract one window height from the total height to calculate totalScrollableDistance
+    const totalScrollableDistance = pageHeight - window.innerHeight;
+
+    // calculate progress bar
+    const percentage = pixelsScrolled / totalScrollableDistance;
+    progressTag.style.width = `${percentage * 100}%`;
+});
